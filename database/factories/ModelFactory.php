b32276e4d2fd5ca1ebde900069b16c316ac47028
Ahmet Ozisik <ozisikahmet@gmail.com>
2015-08-08 15:06:27 +0300

    added classified factory for testing

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,9 +13,29 @@
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
         'email' => $faker->email,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
     ];
+});
+
+$factory->define(App\Classified::class, function($faker) {
+
+	$user = factory(App\User::class)->create();
+	$category = App\Category::whereNotNull('parent_id')->orderBy(DB::raw('RAND()'))->take(1)->first();
+
+	return [
+		'user_id' => $user->id,
+		'category_id' => $category->id,
+		'title' => $faker->name,
+		'description' => $faker->paragraph(5),
+		'price' => $faker->numberBetween(0, 5000),
+		'quantity' => $faker->numberBetween(0, 10),
+		'is_approved' => 1,
+		'visits' => $faker->numberBetween(0, 25000),
+		'photo' => $faker->image(),
+		'expires_at' => Carbon\Carbon::now()->addDays(30)
+	];
 });
